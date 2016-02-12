3e86297eca674ac5aa85913384033208a1f0ef1a
chisun.joung <chisun.joung@lge.com>
2016-02-12 17:48:06 +0900

    List

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,7 +63,8 @@ angular.module('conFusion.controllers', [])
   }
 })
 
-  .controller('MenuController', ['$scope', 'menuFactory','baseURL', function($scope, menuFactory, baseURL) {
+  .controller('MenuController', ['$scope', 'menuFactory','favoriteFactory','baseURL','$ionicListDelegate', function($scope, menuFactory, favoriteFactory, baseURL,$ionicListDelegate
+) {
 
     $scope.baseURL = baseURL;
     $scope.tab = 1;
@@ -105,6 +106,12 @@ angular.module('conFusion.controllers', [])
 
     $scope.toggleDetails = function() {
       $scope.showDetails = !$scope.showDetails;
+    };
+
+    $scope.addFavorite = function(index){
+      console.log("index is " + index);
+      favoriteFactory.addToFavorites(index);
+      $ionicListDelegate.closeOptionButtons();
     };
   }])
 
