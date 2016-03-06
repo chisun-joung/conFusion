ebfe53777de6e0b068be27c5a98725cc357c0a26
chisun-joung <csjoung@gmail.com>
2016-03-06 13:50:49 +0900

    localstorage

--- www/js/services.js
+++ www/js/services.js
@@ -27,15 +27,16 @@ angular.module('conFusion.services',['ngResource'])
             return $resource(baseURL+"feedback/:id");
 
         }])
-        .factory('favoriteFactory',['$resource', 'baseURL',function($resource, baseURL){
+        .factory('favoriteFactory',['$resource', 'baseURL','$localStorage',function($resource, baseURL, $localStorage){
           var favFac = {};
-          var favorites = [];
+          var favorites = $localStorage.getObject('favorites','[]');
           favFac.addToFavorites = function(index) {
             for(var i = 0; i < favorites.length; i++) {
               if(favorites[i].id == index)
                 return;
             }
             favorites.push({id: index});
+            $localStorage.storeObject('favorites',favorites);
           };
 
           favFac.deleteFromFavorites = function(index) {
@@ -44,6 +45,8 @@ angular.module('conFusion.services',['ngResource'])
                 favorites.splice(i, 1);
               }
             }
+
+            $localStorage.storeObject('favorites',favorites);
           }
 
           favFac.getFavorites = function() {
